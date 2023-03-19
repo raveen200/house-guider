e6e635909d52de97f35c14a0f7df9b7fcd8065b3
raveen200 <raveensamudika@gmail.com>
2023-03-19 21:40:10 +0530

    19

--- src/components/signUp/Signup.jsx
+++ src/components/signUp/Signup.jsx
@@ -1,9 +1,32 @@
 import { async } from "@firebase/util";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 import React, { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
+import { motion } from "framer-motion";
+import { UseStateValue } from "../context/StateProvider";
+import { actionTypes } from "../context/reducer";
+
 
 export const firebaseAuth = getAuth();
+const provider = new GoogleAuthProvider();
+//  const [{ user }, dispatch] = UseStateValue();
+
+
+const login = async () => {
+  const {
+    user: { providerData },
+  } = await signInWithPopup(firebaseAuth, provider);
+  // {
+  //   type: actionTypes.SET_USER,
+  //   user: providerData[0],
+  // };
+  localStorage.setItem("user", JSON.stringify(providerData[0]));
+};
 
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
   if (!email || !password) return;
@@ -19,9 +42,9 @@ const defaultFormFeild = {
 
 const Signup = () => {
   const [formFeild, setFormFeild] = useState(defaultFormFeild);
-  const { userName, email, password,confirmPassword } = formFeild;
+  const { userName, email, password, confirmPassword } = formFeild;
 
-const handleSubmit = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (password !== confirmPassword) {
@@ -30,21 +53,21 @@ const handleSubmit = async (e) => {
     }
 
     try {
-        const {user} = await createAuthUserWithEmailAndPassword(email, password);
+      const { user } = await createAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+    } catch (error) {
+      console.log("user Creation an Error", error);
     }
-    catch (error) {
-
-        console.log( 'user Creation an Error',error);
-
-    };
-}
-
-
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormFeild({ ...formFeild, [name]: value });
   };
+
+  console.log(user);
 
   return (
     <section className="h-screen">
@@ -62,7 +85,10 @@ const handleSubmit = async (e) => {
             <div class="flex flex-col text-lg gap-2 mb-4 text-green-900 font-bold items-center justify-center w-full">
               Sign Up with Email and Password
             </div>
-            <form onSubmit={handleSubmit} className="bg-gray-100 rounded-lg px-5 ">
+            <form
+              onSubmit={handleSubmit}
+              className="bg-gray-100 rounded-lg px-5 "
+            >
               {/* Display Name */}
 
               <div class="relative mb-6 " data-te-input-wrapper-init>
@@ -72,22 +98,10 @@ const handleSubmit = async (e) => {
                   name="userName"
                   value={userName}
                   type="text"
-                  class="text-base font-semibold   peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[2.15] 
-                outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 
-                motion-reduce:transition-none dark:text-lime-600 dark:placeholder:text-lime-600 
-                [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                  class="text-base font-semibold   peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[2.15]"
                   id="exampleFormControlInput3"
-                  placeholder="UserName"
+                  placeholder="User Name"
                 />
-                <label
-                  for="exampleFormControlInput3"
-                  class="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15]
-                 text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8]
-                  peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8]
-                   motion-reduce:transition-none dark:text-lime-600 dark:peer-focus:text-lime-600 font-semibold"
-                >
-                  User Name
-                </label>
               </div>
 
               {/* Email */}
@@ -98,22 +112,10 @@ const handleSubmit = async (e) => {
                   name="email"
                   value={email}
                   type="email"
-                  class="text-base font-semibold   peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[2.15] 
-                outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 
-                motion-reduce:transition-none dark:text-lime-600 dark:placeholder:text-lime-600 
-                [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                  class="text-base font-semibold   peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[2.15]"
                   id="exampleFormControlInput3"
                   placeholder="Email address"
                 />
-                <label
-                  for="exampleFormControlInput3"
-                  class="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15]
-                 text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8]
-                  peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8]
-                   motion-reduce:transition-none dark:text-lime-600 dark:peer-focus:text-lime-600 font-semibold"
-                >
-                  Email address
-                </label>
               </div>
 
               {/* Password */}
@@ -125,25 +127,10 @@ const handleSubmit = async (e) => {
                   name="password"
                   value={password}
                   type="password"
-                  class="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[2.15] 
-                outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 
-                motion-reduce:transition-none
-                 dark:text-lime-600 dark:placeholder:text-lime-600 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0
-                font-semibold"
+                  class="text-base font-semibold   peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[2.15]"
                   id="exampleFormControlInput33"
                   placeholder="Password"
                 />
-                <label
-                  for="exampleFormControlInput33"
-                  class="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 
-                transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8]
-                 peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] 
-                peer-data-[te-input-state-active]:scale-[0.8]
-                 motion-reduce:transition-none dark:text-lime-600 dark:peer-focus:text-lime-600
-                 font-semibold"
-                >
-                  Password
-                </label>
               </div>
 
               {/* Confirm Password */}
@@ -155,33 +142,14 @@ const handleSubmit = async (e) => {
                   name="confirmPassword"
                   value={confirmPassword}
                   type="password"
-                  class="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[2.15] 
-                outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 
-                motion-reduce:transition-none
-                 dark:text-lime-600 dark:placeholder:text-lime-600 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0
-                font-semibold"
+                  class="text-base font-semibold   peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[2.15]"
                   id="exampleFormControlInput33"
-                  placeholder="ConfirmPassword"
+                  placeholder="Confirm Password"
                 />
-                <label
-                  for="exampleFormControlInput33"
-                  class="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 
-                transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8]
-                 peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] 
-                peer-data-[te-input-state-active]:scale-[0.8]
-                 motion-reduce:transition-none dark:text-lime-600 dark:peer-focus:text-lime-600
-                 font-semibold"
-                >
-                 Confirm Password
-                </label>
               </div>
 
-
-
-
-
-
-              <button
+              <motion.button
+                whileTap={{ scale: 0.9 }}
                 type="submit"
                 class="inline-block w-full rounded bg-green-600 px-7 pt-3 pb-2.5 text-sm font-medium uppercase leading-normal
                text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 
@@ -192,7 +160,7 @@ const handleSubmit = async (e) => {
                 data-te-ripple-color="light"
               >
                 Sign Up
-              </button>
+              </motion.button>
 
               <div
                 class="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t
@@ -202,20 +170,23 @@ const handleSubmit = async (e) => {
                   OR
                 </p>
               </div>
-              <div
+              <motion.div
+                whileTap={{ scale: 0.9 }}
                 class="mb-3 flex w-full gap-3 items-center justify-center rounded bg-info px-7 pt-3 pb-2.5 text-center text-sm font-semibold uppercase leading-normal
                        text-green-700 shadow-[0_4px_9px_-4px_#54b4d3] transition duration-150 ease-in-out
                        hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:bg-info-600
                         focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)]
                        focus:outline-none focus:ring-0 active:bg-info-700 active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)]"
-                href="#!"
+                onClick={login}
                 role="button"
                 data-te-ripple-init
                 data-te-ripple-color="light"
               >
                 <FcGoogle className="text-2xl" />
                 Continue with Google
-              </div>
+              </motion.div>
+
+              
             </form>
           </div>
         </div>
