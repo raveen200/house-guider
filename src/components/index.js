a5912006ce7e62322caebe2f69c03ff4e380f6ee
raveen200 <raveensamudika@gmail.com>
2023-03-26 19:49:25 +0530

    rowContainer Edit

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,7 @@ export { default as MainContainer } from "./mainContainer/MainContainer";
 export { default as CreateContainer } from "./createContainer/CreateContainer";
 export { default as HomeContainer } from "./homeContainer/HomeContainer";
 export { default as Loader } from "./loader/Loader";
-export { default as RowContainer } from "./rowContainer/RowContainer";
+export { default as RowContainer } from "./rowContainer/index";
 export { default as MenuContainer } from "../components/menuContainer/MenuContainer";
 export { default as Footer } from "../components/footer/Footer";
 export { default as Login } from "../components/login/Login";
--- src/components/menuContainer/MenuContainer.jsx
+++ src/components/menuContainer/MenuContainer.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { BsHouseAddFill } from "react-icons/bs";
 import { provinces } from "../../utils/data";
 import { motion } from "framer-motion";
-import RowContainer from "../rowContainer/RowContainer";
+import RowContainer from "../rowContainer";
 import { UseStateValue } from "../context/StateProvider";
 
 const MenuContainer = () => {
